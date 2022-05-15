f1d43c1ff6b39e5caabdd72953b0192e7a189e29
Sim <https://github.com/VladiNaumov>
2022-05-15 14:20:13 +0300

    Gb

---
+++ src/com/naumDeveloper/javaCore2/unit_6/Server/original/ClientHandler__.java
@@ -0,0 +1,77 @@
+package com.naumDeveloper.javaCore2.unit_6.Server.original;
+
+import org.example.Server;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+public class ClientHandler__ {
+    /* Класс отвечающий за авторизацию клеента к сервера и создание отдельного потока подключения к серверу */
+    private Server server;
+    private Socket socket;
+    private DataInputStream in;
+    private DataOutputStream out;
+    private String username;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public ClientHandler__(Server server, Socket socket) throws IOException {
+        this.server = server;
+        this.socket = socket;
+        this.in = new DataInputStream(socket.getInputStream());
+        this.out = new DataOutputStream(socket.getOutputStream());
+
+        /*создание отдельного потока подключения к серверу (Authentication server)*/
+        new Thread(() -> {
+            try {
+                // Цикл авторизации
+                while (true) {
+                    String msg = in.readUTF();
+                    if (msg.startsWith("/login ")) {
+                        // login Bob
+                        String usernameFromLogin = msg.split("\\s")[1];
+
+                        if (server.isNickBusy(usernameFromLogin)) {
+                            sendMessage("/login_failed Current nickname is already used");
+                            continue;
+                        }
+
+                        username = usernameFromLogin;
+                        sendMessage("/login_ok " + username);
+                        server.subscribe(this);
+                        break;
+                    }
+                }
+                // Цикл общения с клиентом
+                while (true) {
+                    String msg = in.readUTF();
+                    server.broadcastMessage(username + ": " + msg);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                disconnect();
+            }
+        }).start();
+    }
+
+    public void sendMessage(String message) throws IOException {
+        out.writeUTF(message);
+    }
+
+    public void disconnect() {
+       // server.unsubscribe(this);
+        if (socket != null) {
+            try {
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
