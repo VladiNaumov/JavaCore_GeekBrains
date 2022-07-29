0f1c6318084f24aca04650c87ddc7d4481659f81
Sim <https://github.com/VladiNaumov>
2022-07-29 20:55:51 +0300

    Gb

--- ChatServer/MiniClientFx/src/main/java/module-info.java
+++ ChatServer/MiniClientFx/src/main/java/module-info.java
@@ -1,8 +1,8 @@
-module com.example{
+module com.example.miniclientfx {
     requires javafx.controls;
     requires javafx.fxml;
 
+
     opens com.example to javafx.fxml;
     exports com.example;
-
- }
+}
