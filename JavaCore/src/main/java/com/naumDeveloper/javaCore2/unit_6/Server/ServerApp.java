974ef515bb895973d26c7ff4bb45f54e6860878b
Sim <https://github.com/VladiNaumov>
2022-07-30 15:33:38 +0300

    Gb

--- JavaCore/src/main/java/com/naumDeveloper/javaCore2/unit_6/Server/ServerApp.java
+++ JavaCore/src/main/java/com/naumDeveloper/javaCore2/unit_6/Server/ServerApp.java
@@ -1,7 +1,33 @@
 package com.naumDeveloper.javaCore2.unit_6.Server;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class ServerApp {
+
     public static void main(String[] args) {
-        new Server(8189);
+        try (ServerSocket serverSocket = new ServerSocket(8189)) {
+            System.out.println("Сервер запущен на порту 8189. Ожидаем подключение клиента...");
+            Socket socket = serverSocket.accept();
+            DataInputStream in = new DataInputStream(socket.getInputStream());
+            DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+            System.out.println("Клиент подключился");
+
+            //  цикл обмена сообщения
+            while (true) {
+                //получение сообщения от клиента
+                String msg = in.readUTF();
+
+                System.out.println(msg);
+
+                // отправка сообщение клиенту
+                out.writeUTF("ECHO: " + msg);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
---
+++ JavaCore/src/main/java/com/naumDeveloper/javaCore2/unit_7/ServerApp.java
@@ -0,0 +1,7 @@
+package com.naumDeveloper.javaCore2.unit_7;
+
+public class ServerApp {
+    public static void main(String[] args) {
+        new Server(8189);
+    }
+}
